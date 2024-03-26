2080fa9cc9441a790e16713ff61c78cc2c7abc88
yeyulchoi <yeyulchoi@outlook.com>
2024-03-26 01:26:29 -0400

    removed email notification(commented)& replaced with mailto link

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ import secrets
 
 
 app = Flask(__name__)
-app.config['MAIL_SERVER']='smtp.gmail.com'
-app.config['MAIL_PORT']=465
-app.config['MAIL_USE_TLS']=False
-app.config['MAIL_USE_SSL']=True
-app.config['MAIL_USERNAME']='yeyulchoi@gmail.com'  #os.environ.get('FLASK_MAIL_USER_EMAIL')
-app.config['MAIL_PASSWORD']='crlr kkkp wkbx xbpw'  #os.environ.get('FLASK_MAIL_USER_PASSWORD')
-mail = Mail(app)
+# app.config['MAIL_SERVER']='smtp.gmail.com'
+# app.config['MAIL_PORT']=465
+# app.config['MAIL_USE_TLS']=False
+# app.config['MAIL_USE_SSL']=True
+# app.config['MAIL_USERNAME']='yeyulchoi@gmail.com'  #os.environ.get('FLASK_MAIL_USER_EMAIL')
+# app.config['MAIL_PASSWORD']='crlr kkkp wkbx xbpw'  #os.environ.get('FLASK_MAIL_USER_PASSWORD')
+# mail = Mail(app)
 
 
 app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:''@localhost/flask_aws2'
@@ -22,14 +22,14 @@ app.config['SECRET_KEY']=secrets.token_hex(16)
 
 @app.route('/', methods=['GET','POST'])
 def home():
-    if request.method =='POST':
-        msg =Message(
-            "This is from flask-resume-app",
-            sender='noreply@demo.com',
-            recipients=['yeyulchoi@gmail.com','yeyulchoi@outlook.com'])
-        msg.body =" Hey, How are you. I just clicked your email on the resume!!"
-        mail.send(msg)
-        redirect('index.html')
+    # if request.method =='POST':
+    #     msg =Message(
+    #         "This is from flask-resume-app",
+    #         sender='noreply@demo.com',
+    #         recipients=['yeyulchoi@gmail.com','yeyulchoi@outlook.com'])
+    #     msg.body =" Hey, How are you. I just clicked your email on the resume!!"
+    #     mail.send(msg)
+    #     redirect('index.html')
     return render_template('index.html')
 
 
